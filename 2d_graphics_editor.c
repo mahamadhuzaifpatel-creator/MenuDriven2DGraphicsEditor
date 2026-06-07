610e7a0b85529145ce7a32047a8152e6f69a5153
mahamadhuzaifpatel-creator <mahamadhuzaifpatel@gmail.com>
2026-06-07 22:51:23 +0530

    Day 2: Implemented line and rectangle drawing

--- 2d_graphics_editor.c
+++ 2d_graphics_editor.c
@@ -22,9 +22,62 @@ void displayCanvas()
     }
 }
 
+void drawLine()
+{
+    int row,startCol,endCol,i;
+
+    printf("Enter row: ");
+    scanf("%d",&row);
+
+    printf("Enter start column: ");
+    scanf("%d",&startCol);
+
+    printf("Enter end column: ");
+    scanf("%d",&endCol);
+
+    for(i=startCol;i<=endCol;i++)
+    {
+        canvas[row][i]='*';
+    }
+}
+
+void drawRectangle()
+{
+    int row,col,height,width;
+    int i;
+
+    printf("Enter top row: ");
+    scanf("%d",&row);
+
+    printf("Enter left column: ");
+    scanf("%d",&col);
+
+    printf("Enter height: ");
+    scanf("%d",&height);
+
+    printf("Enter width: ");
+    scanf("%d",&width);
+
+    for(i=col;i<col+width;i++)
+    {
+        canvas[row][i]='*';
+        canvas[row+height-1][i]='*';
+    }
+
+    for(i=row;i<row+height;i++)
+    {
+        canvas[i][col]='*';
+        canvas[i][col+width-1]='*';
+    }
+}
 int main()
 {
     initializeCanvas();
+
+    drawLine();
+    drawRectangle();
+
     displayCanvas();
+
     return 0;
 }
